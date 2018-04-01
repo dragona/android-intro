534e24177571e262cb0700630e6904c52bc752a5
Feilong <feilong.dragona@gmail.com>
2018-04-01 21:06:33 +0800

    Intent Extras

---
+++ IntentExtras/app/src/main/java/mg/studio/intentpassdata/MainActivity.java
@@ -0,0 +1,23 @@
+package mg.studio.intentpassdata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnClick(View view) {
+        String value = "Hello World! - This message was from the first activity.";
+        Intent intent = new Intent(getApplicationContext(), NewActivity.class);
+        intent.putExtra("keyword", value);
+        startActivity(intent);
+    }
+}
---
+++ IntentExtras/app/src/main/java/mg/studio/intentpassdata/NewActivity.java
@@ -0,0 +1,36 @@
+package mg.studio.intentpassdata;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+
+
+public class NewActivity extends AppCompatActivity {
+    private String value;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_two);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            value = bundle.getString("keyword");
+        }
+
+        ((TextView) findViewById(R.id.display)).setText(value);
+    }
+}
