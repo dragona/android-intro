20761c0be986a3c352586b1d697f46fafc135f3e
Feilong <feilong.dragona@gmail.com>
2019-09-06 17:10:47 +0800

    xandroid

--- 24_Fingerprint/app/src/main/java/mg/studio/fingerprint/MainActivity.java
+++ 24_Fingerprint/app/src/main/java/mg/studio/fingerprint/MainActivity.java
@@ -11,7 +11,7 @@ package mg.studio.fingerprint;
  *
  */
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
