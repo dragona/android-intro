2d300a950eb6c2eb8748b8c374e42136df259ca9
Feilong <feilong.dragona@gmail.com>
2019-09-06 16:47:23 +0800

    xandroid

--- 19_DataBase/app/src/main/java/mg/studio/database/MainActivity.java
+++ 19_DataBase/app/src/main/java/mg/studio/database/MainActivity.java
@@ -12,7 +12,7 @@ package mg.studio.database;
  */
 
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
