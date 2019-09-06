7539f45a2352d0bc9523bf5f3caf9f8cc172826b
Feilong <feilong.dragona@gmail.com>
2019-09-06 17:25:33 +0800

    xandroid

--- 27_IntentExtras/app/src/main/java/mg/studio/intentpassdata/MainActivity.java
+++ 27_IntentExtras/app/src/main/java/mg/studio/intentpassdata/MainActivity.java
@@ -1,7 +1,7 @@
 package mg.studio.intentpassdata;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
