e01a58df3ab300678458e56de288574379bb7806
Feilong <feilong.dragona@gmail.com>
2019-09-06 17:06:13 +0800

    migrate to xandroid

--- 23_Service/app/src/main/java/mg/studio/service/MainActivity.java
+++ 23_Service/app/src/main/java/mg/studio/service/MainActivity.java
@@ -14,7 +14,7 @@ package mg.studio.service;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
