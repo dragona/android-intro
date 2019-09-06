92d54d0132f44e34e47007ebe2593ebf87376d20
Feilong <feilong.dragona@gmail.com>
2019-09-06 16:20:11 +0800

    xandroid

--- 11_GradientBackground/app/src/main/java/mg/studio/gradientbackground/MainActivity.java
+++ 11_GradientBackground/app/src/main/java/mg/studio/gradientbackground/MainActivity.java
@@ -11,7 +11,7 @@ package mg.studio.gradientbackground;
  *
  */
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
