4430ebcad4a559f7e0e3f570ace9967ed6665773
studio <---@--->
2023-02-26 15:55:03 +0800

    Widgets

--- starter/ActivityDemo/app/src/main/java/mg/x261/activitydemo/MainActivity.java
+++ starter/ActivityDemo/app/src/main/java/mg/x261/activitydemo/MainActivity.java
@@ -10,23 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_relative);
+        setContentView(R.layout.activity_main);
 
 
-        // Create a new LinearLayout
-        LinearLayout linearLayout = new LinearLayout(this);
-        // Set the orientation of the LinearLayout to vertical
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        // Create the first TextView
-        TextView textView1 = new TextView(this);
-        textView1.setText("This is the first TextView");
-        // Create the second TextView
-        TextView textView2 = new TextView(this);
-        textView2.setText("This is the second TextView");
-        // Add the TextViews to the LinearLayout
-        linearLayout.addView(textView1);
-        linearLayout.addView(textView2);
-        // Set the content view of the activity to the LinearLayout
-        setContentView(linearLayout);
     }
 }
